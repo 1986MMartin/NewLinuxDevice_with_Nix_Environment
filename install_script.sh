badbb1115e69eb95205c6ef8f38e46220a65d009
Markus Martin <1986mmartin@gmail.com>
2022-02-06 18:52:42 +0000

    new line in File 'install_script.sh'

--- install_script.sh
+++ install_script.sh
@@ -4,6 +4,22 @@ curl -L https://nixos.org/nix/install | sh
 # source nix
 . ~/.nix-profile/etc/profile.d/nix.sh
 
+# install packages
+nix-env -iA \
+	nixpkgs.zsh \
+	nixpkgs.antibody \
+	nixpkgs.git \
+	nixpkgs.neovim \
+	nixpkgs.tmux \
+	nixpkgs.stow \
+	nixpkgs.yarn \
+	nixpkgs.fzf \
+	nixpkgs.ripgrep \
+	nixpkgs.bat \
+	nixpkgs.gnumake \
+	nixpkgs.gcc \
+	nixpkgs.direnv
+
 # stow dotfiles
 stow git
 stow nvim
